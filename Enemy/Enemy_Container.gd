3bec1c26422778c696a25c01859fcc9670c5b196
Allan-Mance <mance.allan@gmail.com>
2023-11-10 13:41:30 -0500

    Finish Assignment

--- Enemy/Enemy_Container.gd
+++ Enemy/Enemy_Container.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 @onready var monster = load("res://Enemy/monster.tscn/Player.tscn")
-var starting_position = Vector2(200,200)
+var starting_position = Vector2(300,300)
 
 func _physics_process(_delta):
 	if not has_node("Monster"):
--- Enemy/monster.gd
+++ Enemy/monster.gd
@@ -1,1 +1,21 @@
-extends AnimatedSprite2D
+extends CharacterBody2D
+
+const GRAVITY = 500.0
+const JUMP_FORCE = -300.0
+const MOVE_SPEED = 100.0
+
+
+func _process(delta):
+	velocity.y += GRAVITY * delta
+#	var player = get_node("/root/Game/Node2D/Player")
+#	if player:
+#		var direction = (player.global_position - global_position).normalized()
+
+#		velocity.x = direction.x * MOVE_SPEED
+	#	velocity.y = JUMP_FORCE
+#		$AnimatedSprite.play("Jumping")
+	#	$AnimatedSprite.flip_h = direction.x < 0
+
+#	else:
+#		velocity.x = 0
+#		$AnimatedSprite.play("Idle")
